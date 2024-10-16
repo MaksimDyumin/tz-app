e2cc017affde697638c8643d820d83d577480c80
Maxim Dyumin <maxdyumin2000@yandex.ru>
2024-10-17 02:08:20 +0300

    create Users page

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="d-flex flex-wrap user-card-container">
+    <UserCard class="user-card" v-for="user in users" :key="user.id" :user="user" />
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import UserCard from '@/components/UserCard.vue'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
-  name: 'HomeView',
+  name: 'UserView',
   components: {
-    HelloWorld
+    UserCard
+  },
+  async mounted() {
+    await this.getUsers()
+  },
+  computed: {
+    ...mapGetters({
+      users: 'users/users'
+    })
+  },
+  methods: {
+    ...mapActions({
+      getUsers: 'users/getUsers'
+    })
   }
 }
 </script>
+
+<style lang="scss">
+.user-card-container {
+  gap: 20px;
+  justify-content: flex-start!important;
+
+  .user-card {
+    flex: 0 1 31.33%;
+    box-sizing: border-box;
+  }
+}
+
+@media (max-width: 992px) {
+  .user-card{
+    flex: 0 1 48%!important;
+  }
+}
+
+@media (max-width: 650px) {
+  .user-card{
+    flex: 0 1 100%!important;
+  }
+}
+</style>
