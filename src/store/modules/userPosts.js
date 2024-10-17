7ad5a913294e9a374935f9d48dbc6b9736c0f57f
Maxim Dyumin <maxdyumin2000@yandex.ru>
2024-10-17 04:27:32 +0300

    create userPosts store module

---
+++ src/store/modules/userPosts.js
@@ -0,0 +1,51 @@
+/* eslint quotes: ["warn", "single"] */
+
+import { publickClient } from '@/axiosInstances/publickClient'
+
+
+export const namespaced = true
+
+export const state = {
+  userPosts: null,
+  postComments: null
+}
+
+export const getters = {
+  userPosts(state) {
+    return state.userPosts
+  },
+  postComments(state) {
+    return state.postComments
+  }
+}
+
+export const mutations = {
+  SET_USER_POSTS(state, userPosts) {
+    state.userPosts = userPosts
+  },
+  SET_POST_COMMENTS(state, postComments) {
+    state.postComments = postComments
+  }
+}
+
+export const actions = {
+  async getUserPosts({ commit }, userId) {
+    try {
+      const response = await publickClient.get(`posts/?userId=${userId}`)
+      commit('SET_USER_POSTS', response.data)
+    } catch (error) {
+      console.error(error)
+      // this._vm.$toast.error(error?.response?.data?.detail);
+    }
+  },
+  async getPostComments({ commit }, postId) {
+    try {
+      const response = await publickClient.get(`comments/?postId=${postId}`)
+      commit('SET_POST_COMMENTS', response.data)
+    } catch (error) {
+      console.error(error)
+      // this._vm.$toast.error(error?.response?.data?.detail);
+    }
+  }
+}
+
