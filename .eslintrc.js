5a8ad2acad5520c0f6b2ba6ea3271350921c89b1
Maxim Dyumin <maxdyumin2000@yandex.ru>
2024-10-17 05:19:07 +0300

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-multiple-empty-lines': 0,
+    'space-before-function-paren': 0,
+    'quotes': 0
+  }
+}
